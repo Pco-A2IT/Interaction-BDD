709d5dc9fdc09f76de4dbeebf9303fea29b8cd05
luigi-bardi <luigi.bardi@insa-lyon.fr>
2016-12-21 18:13:38 +0100

    Update AjoutBDD_dossierPatient.php

--- AjoutBDD_dossierPatient.php
+++ AjoutBDD_dossierPatient.php
@@ -10,9 +10,19 @@ catch(Exception $e)
 }
 
 // Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO patient(id_patient,nom, prenom,civilite,num_telephone,ville,code_postal,adresse,date_creation_dossier,scan_cerebral,angioscan,bilan_biologique,bilan_cardiaque,traitement_termine) VALUES(NULL,?, ?, \'Mr\',?, ?, \'77176\',\'24 rue de ormeteau\',\'2016-12-08\', \'YES\',\'NO\',\'YES\',\'YES\',\'NO\')'); // ici le ? correspond à la valeur que l'on rentre dans le formulaire
-$req->execute(array($_POST['nom'],$_POST['prenom'],$_POST['num_telephone'] ,$_POST['ville']));
+$req = $bdd->prepare('INSERT INTO patient(id_patient,nom, prenom,civilite,num_telephone,ville,code_postal,adresse,date_creation_dossier,scan_cerebral,angioscan,bilan_biologique,bilan_cardiaque,traitement_termine) VALUES(NULL,?, ?,?,?, ?, ?, ?,\'2016-12-08\', \'YES\',\'NO\',\'YES\',\'YES\',\'NO\')'); // ici le ? correspond à la valeur que l'on rentre dans le formulaire
+$req->execute(array($_POST['nom'], $_POST['prenom'],$_POST['civilite'],$_POST['num_telephone'] ,$_POST['ville'],$_POST['code_postal'],$_POST['adresse']));
+
+
+$req = $bdd->query("SELECT * FROM patient");
+while ($donnees = $req->fetch())
+{
+    $nom = $donnees['nom'];
+    
+}
+echo $_POST['civilite'];
 
 // Redirection du visiteur vers la page du minichat
+
 header('Location: Dossier_Patient.php');
 ?>
