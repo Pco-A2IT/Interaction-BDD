65218486d987c9aefcfe1307c88309a7619f33d7
luigi-bardi <luigi.bardi@insa-lyon.fr>
2016-12-22 10:51:26 +0100

    Date_Naissance et Date_Création

--- AjoutBDD_dossierPatient.php
+++ AjoutBDD_dossierPatient.php
@@ -10,8 +10,8 @@ catch(Exception $e)
 }
 
 // Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO patient(id_patient,nom, prenom,civilite,num_telephone,ville,code_postal,adresse,date_creation_dossier,scan_cerebral,angioscan,bilan_biologique,bilan_cardiaque,traitement_termine) VALUES(NULL,?, ?,?,?, ?, ?, ?,\'2016-12-08\', \'YES\',\'NO\',\'YES\',\'YES\',\'NO\')'); // ici le ? correspond à la valeur que l'on rentre dans le formulaire
-$req->execute(array($_POST['nom'], $_POST['prenom'],$_POST['civilite'],$_POST['num_telephone'] ,$_POST['ville'],$_POST['code_postal'],$_POST['adresse']));
+$req = $bdd->prepare('INSERT INTO patient(id_patient,nom, prenom,civilite,date_naissance,num_telephone,ville,code_postal,adresse,date_creation_dossier,scan_cerebral,angioscan,bilan_biologique,bilan_cardiaque,traitement_termine) VALUES(NULL,?, ?,?,? ,?, ?, ?, ?,NOW(), \'YES\',\'NO\',\'YES\',\'YES\',\'NO\')'); // ici le ? correspond à la valeur que l'on rentre dans le formulaire
+$req->execute(array($_POST['nom'], $_POST['prenom'],$_POST['civilite'],  $_POST['date10_annee'].'-'. $_POST['date10_mois'].'-'. $_POST['date10_jour'],$_POST['num_telephone'] ,$_POST['ville'],$_POST['code_postal'],$_POST['adresse']));
 
 
 $req = $bdd->query("SELECT * FROM patient");
